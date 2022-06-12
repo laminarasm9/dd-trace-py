laminarasm9/dd-trace-py

commit 1fd57b39c75266fd82f16357b7902fba7b2cd10a

fix(fastapi/starlette): provide correct root span resource names for sub-applications (#3612)
## Description
Previously when you have a [FastAPI sub-application](https://fastapi.tiangolo.com/advanced/sub-applications/) the root span resource name will only be for the mounted route and not the full route. With this change the tracer uses the full route.

To fix this I wrapped the FastAPI and Starlette handlers. [By wrapping the handlers I was able to grab the path value off of each individual `handle` call, join them together, and set them as the resource name on the root span. This avoids getting a random url that doesn't route anywhere (avoiding infinite cardinality) because the path value is already verified by Starlette.](https://github.com/DataDog/dd-trace-py/blob/16864158f02bc37c6b34af2f1ae55463371210e4/ddtrace/contrib/starlette/patch.py#L66-L92)

## Checklist
- [x] Title must conform to [conventional commit](https://github.com/conventional-changelog/commitlint/tree/master/%40commitlint/config-conventional).
- [x] Add additional sections for `feat` and `fix` pull requests.
- [ ] Ensure tests are passing for affected code.
- [x] [Library documentation](https://github.com/DataDog/dd-trace-py/tree/1.x/docs) and/or [Datadog's documentation site](https://github.com/DataDog/documentation/) is updated. Link to doc PR in description.


## Relevant issue(s)
Original GH issue: https://github.com/DataDog/dd-trace-py/issues/3061

## Testing strategy
I added snapshot and normal span tests checking values, for when Starlette or FastAPI use a sub-app. 


## Checklist
- [ ] Added to the correct milestone.
- [ ] Tests provided or description of manual testing performed is included in the code or PR.
- [ ] Library documentation is updated.
- [ ] [Corp site](https://github.com/DataDog/documentation/) documentation is updated (link to the PR).

ddtrace/contrib/starlette/patch.py
@@ -1,11 +1,15 @@
+from typing import List
+
 import starlette
 from starlette.middleware import Middleware
-from starlette.routing import Match
 
 from ddtrace import config
 from ddtrace.contrib.asgi.middleware import TraceMiddleware
 from ddtrace.internal.logger import get_logger
+from ddtrace.internal.utils import get_argument_value
 from ddtrace.internal.utils.wrappers import unwrap as _u
+from ddtrace.span import Span
+from ddtrace.vendor.wrapt import ObjectProxy
 from ddtrace.vendor.wrapt import wrap_function_wrapper as _w
 
 
@@ -22,28 +26,9 @@ config._add(
 )
 
 
-def get_resource(scope):
-    path = None
-    routes = scope["app"].routes
-    for route in routes:
-        match, _ = route.matches(scope)
-        if match == Match.FULL:
-            path = route.path
-            break
-        elif match == Match.PARTIAL and path is None:
-            path = route.path
-    return path
-
-
-def span_modifier(span, scope):
-    resource = get_resource(scope)
-    if config.starlette["aggregate_resources"] and resource:
-        span.resource = "{} {}".format(scope["method"], resource)
-
-
 def traced_init(wrapped, instance, args, kwargs):
     mw = kwargs.pop("middleware", [])
-    mw.insert(0, Middleware(TraceMiddleware, integration_config=config.starlette, span_modifier=span_modifier))
+    mw.insert(0, Middleware(TraceMiddleware, integration_config=config.starlette))
     kwargs.update({"middleware": mw})
 
     wrapped(*args, **kwargs)
@@ -57,6 +42,12 @@ def patch():
 
     _w("starlette.applications", "Starlette.__init__", traced_init)
 
+    # We need to check that Fastapi instrumentation hasn't already patched these
+    if not isinstance(starlette.routing.Route.handle, ObjectProxy):
+        _w("starlette.routing", "Route.handle", traced_handler)
+    if not isinstance(starlette.routing.Mount.handle, ObjectProxy):
+        _w("starlette.routing", "Mount.handle", traced_handler)
+
 
 def unpatch():
     if not getattr(starlette, "_datadog_patch", False):
@@ -65,3 +56,54 @@ def unpatch():
     setattr(starlette, "_datadog_patch", False)
 
     _u(starlette.applications.Starlette, "__init__")
+
+    # We need to check that Fastapi instrumentation hasn't already unpatched these
+    if isinstance(starlette.routing.Route.handle, ObjectProxy):
+        _u(starlette.routing.Route, "handle")
+
+    if isinstance(starlette.routing.Mount.handle, ObjectProxy):
+        _u(starlette.routing.Mount, "handle")
+
+
+def traced_handler(wrapped, instance, args, kwargs):
+    # Since handle can be called multiple times for one request, we take the path of each instance
+    # Then combine them at the end to get the correct resource names
+    scope = get_argument_value(args, kwargs, 0, "scope")
+
+    # Add the path to the resource_paths list
+    if "resource_paths" not in scope["datadog"]:
+        scope["datadog"]["resource_paths"] = [instance.path]
+    else:
+        scope["datadog"]["resource_paths"].append(instance.path)
+
+    request_spans = scope["datadog"].get("request_spans", [])  # type: List[Span]
+    resource_paths = scope["datadog"].get("resource_paths", [])  # type: List[str]
+
+    if len(request_spans) == len(resource_paths):
+        # Iterate through the request_spans and assign the correct resource name to each
+        for index, span in enumerate(request_spans):
+            # We want to set the full resource name on the first request span
+            # And one part less of the full resource name for each proceeding request span
+            # e.g. full path is /subapp/hello/{name}, first request span gets that as resource name
+            # Second request span gets /hello/{name}
+            path = "".join(resource_paths[index:])
+
+            if scope.get("method"):
+                span.resource = "{} {}".format(scope["method"], path)
+            else:
+                span.resource = path
+    # at least always update the root asgi span resource name request_spans[0].resource = "".join(resource_paths)
+    elif request_spans and resource_paths:
+        if scope.get("method"):
+            request_spans[0].resource = "{} {}".format(scope["method"], "".join(resource_paths))
+        else:
+            request_spans[0].resource = "".join(resource_paths)
+
+    else:
+        log.debug(
+            "unable to update the request span resource name, request_spans:%r, resource_paths:%r",
+            request_spans,
+            resource_paths,
+        )
+
+    return wrapped(*args, **kwargs)

tests/contrib/starlette/test_starlette.py
@@ -343,7 +343,7 @@ def test_path_param_no_aggregate(client, tracer, test_spans):
     request_span = next(test_spans.filter_spans(name="starlette.request"))
     assert request_span.service == "starlette"
     assert request_span.name == "starlette.request"
-    assert request_span.resource == "GET /users/1"
+    assert request_span.resource == "GET /users/{userid:int}"
     assert request_span.error == 0
     assert request_span.get_tag("http.method") == "GET"
     assert request_span.get_tag("http.url") == "http://testserver/users/1"
@@ -394,6 +394,13 @@ def test_table_query(client, tracer, test_spans):
     assert sql_span.resource == "SELECT * FROM NOTES"
     assert sql_span.error == 0
     assert sql_span.get_tag("sql.db") == "test.db"
+
+
+@snapshot()
+def test_subapp_snapshot(snapshot_client):
+    response = snapshot_client.get("/sub-app/hello/name")
+    assert response.status_code == 200
+    assert response.text == "Success"
 
 
 @snapshot()

ddtrace/contrib/fastapi/patch.py
@@ -5,9 +5,10 @@ import fastapi.routing
 from ddtrace import Pin
 from ddtrace import config
 from ddtrace.contrib.asgi.middleware import TraceMiddleware
-from ddtrace.contrib.starlette.patch import get_resource
+from ddtrace.contrib.starlette.patch import traced_handler
 from ddtrace.internal.logger import get_logger
 from ddtrace.internal.utils.wrappers import unwrap as _u
+from ddtrace.vendor.wrapt import ObjectProxy
 from ddtrace.vendor.wrapt import wrap_function_wrapper as _w
 
 
@@ -24,15 +25,9 @@ config._add(
 )
 
 
-def span_modifier(span, scope):
-    resource = get_resource(scope)
-    if config.fastapi["aggregate_resources"] and resource:
-        span.resource = "{} {}".format(scope["method"], resource)
-
-
 def traced_init(wrapped, instance, args, kwargs):
     mw = kwargs.pop("middleware", [])
-    mw.insert(0, Middleware(TraceMiddleware, integration_config=config.fastapi, span_modifier=span_modifier))
+    mw.insert(0, Middleware(TraceMiddleware, integration_config=config.fastapi))
     kwargs.update({"middleware": mw})
     wrapped(*args, **kwargs)
 
@@ -71,6 +66,13 @@ def patch():
     _w("fastapi.applications", "FastAPI.__init__", traced_init)
     _w("fastapi.routing", "serialize_response", traced_serialize_response)
 
+    # We need to check that Starlette instrumentation hasn't already patched these
+    if not isinstance(fastapi.routing.APIRoute.handle, ObjectProxy):
+        _w("fastapi.routing", "APIRoute.handle", traced_handler)
+
+    if not isinstance(fastapi.routing.Mount.handle, ObjectProxy):
+        _w("starlette.routing", "Mount.handle", traced_handler)
+
 
 def unpatch():
     if not getattr(fastapi, "_datadog_patch", False):
@@ -80,3 +82,10 @@ def unpatch():
 
     _u(fastapi.applications.FastAPI, "__init__")
     _u(fastapi.routing, "serialize_response")
+
+    # We need to check that Starlette instrumentation hasn't already unpatched these
+    if isinstance(fastapi.routing.APIRoute.handle, ObjectProxy):
+        _u(fastapi.routing.APIRoute, "handle")
+
+    if isinstance(fastapi.routing.Mount.handle, ObjectProxy):
+        _u(fastapi.routing.Mount, "handle")

releasenotes/notes/fix-resource-name-when-fastapi-starlette-use-subapps-95ac82b038e6cbf9.yaml
@@ -1,0 +1,4 @@
+---
+fixes:
+  - |
+    fastapi/starlette: when using sub-apps, formerly a call to ``/sub-app/hello/{name}`` would give a resource name of ``/sub-app``. Now the full path ``/sub-app/hello/{name}`` is used for the resource name.

tests/contrib/fastapi/app.py
@@ -102,4 +102,12 @@ def get_app():
             fp.flush()
             return FileResponse(fp.name)
 
+    subapp = FastAPI()
+
+    @subapp.get("/hello/{name}")
+    def hello():
+        return {"Greeting": "Hello"}
+
+    app.mount("/sub-app", subapp)
+
     return app

tests/contrib/fastapi/test_fastapi.py
@@ -8,6 +8,8 @@ import pytest
 import ddtrace
 from ddtrace.contrib.fastapi import patch as fastapi_patch
 from ddtrace.contrib.fastapi import unpatch as fastapi_unpatch
+from ddtrace.contrib.starlette.patch import patch as patch_starlette
+from ddtrace.contrib.starlette.patch import unpatch as unpatch_starlette
 from ddtrace.propagation import http as http_propagation
 from tests.utils import DummyTracer
 from tests.utils import TracerSpanContainer
@@ -487,6 +489,46 @@ def test_service_can_be_overridden(client, tracer, test_spans):
 
     span = spans[0][0]
     assert span.service == "test-override-service"
+
+
+def test_w_patch_starlette(client, tracer, test_spans):
+    patch_starlette()
+    try:
+        response = client.get("/file", headers={"X-Token": "DataDog"})
+        assert response.status_code == 200
+        assert response.text == "Datadog says hello!"
+
+        spans = test_spans.pop_traces()
+        assert len(spans) == 1
+        assert len(spans[0]) == 1
+        request_span = spans[0][0]
+        assert request_span.service == "fastapi"
+        assert request_span.name == "fastapi.request"
+        assert request_span.resource == "GET /file"
+        assert request_span.error == 0
+        assert request_span.get_tag("http.method") == "GET"
+        assert request_span.get_tag("http.url") == "http://testserver/file"
+        assert request_span.get_tag("http.query.string") is None
+        assert request_span.get_tag("http.status_code") == "200"
+    finally:
+        unpatch_starlette()
+
+
+@snapshot()
+def test_subapp_snapshot(snapshot_client):
+    response = snapshot_client.get("/sub-app/hello/name")
+    assert response.status_code == 200
+
+
+@snapshot(token_override="tests.contrib.fastapi.test_fastapi.test_subapp_snapshot")
+def test_subapp_w_starlette_patch_snapshot(snapshot_client):
+    # Test that patching starlette doesn't affect the spans generated
+    patch_starlette()
+    try:
+        response = snapshot_client.get("/sub-app/hello/name")
+        assert response.status_code == 200
+    finally:
+        unpatch_starlette()
 
 
 @snapshot()

tests/contrib/fastapi/test_fastapi_patch.py
@@ -12,11 +12,17 @@ class TestFastapiPatch(PatchTestCase.Base):
     def assert_module_patched(self, fastapi):
         self.assert_wrapped(fastapi.applications.FastAPI.__init__)
         self.assert_wrapped(fastapi.routing.serialize_response)
+        self.assert_wrapped(fastapi.routing.APIRoute.handle)
+        self.assert_wrapped(fastapi.routing.Mount.handle)
 
     def assert_not_module_patched(self, fastapi):
         self.assert_not_wrapped(fastapi.applications.FastAPI.__init__)
         self.assert_not_wrapped(fastapi.routing.serialize_response)
+        self.assert_not_wrapped(fastapi.routing.APIRoute.handle)
+        self.assert_not_wrapped(fastapi.routing.Mount.handle)
 
     def assert_not_module_double_patched(self, fastapi):
         self.assert_not_double_wrapped(fastapi.applications.FastAPI.__init__)
         self.assert_not_double_wrapped(fastapi.routing.serialize_response)
+        self.assert_not_double_wrapped(fastapi.routing.APIRoute.handle)
+        self.assert_not_double_wrapped(fastapi.routing.Mount.handle)

tests/contrib/starlette/app.py
@@ -8,6 +8,7 @@ from starlette.responses import FileResponse
 from starlette.responses import PlainTextResponse
 from starlette.responses import Response
 from starlette.responses import StreamingResponse
+from starlette.routing import Mount
 from starlette.routing import Route
 
 
@@ -117,6 +118,8 @@ def get_app(engine):
         Route("/users/{userid:int}/{attribute:str}", endpoint=success, name="multi_path_params", methods=["GET"]),
         Route("/notes", endpoint=list_notes, methods=["GET"]),
         Route("/notes", endpoint=add_note, methods=["POST"]),
+        Mount("/sub-app", Starlette(routes=[Route("/hello/{name}", endpoint=success, name="200", methods=["GET"])])),
     ]
+
     app = Starlette(routes=routes, on_startup=[database.connect], on_shutdown=[database.disconnect])
     return app

commit 6dd41e588d88a6c37115699557084a584e7a9b7e

fix(starlette): fix error raised when patching happens after creating an app (#3790)
Fixes issue introduced in #3612 for Starlette framework test suite.

```
>       # Add the path to the resource_paths list
>       if "resource_paths" not in scope["datadog"]:
E       KeyError: 'datadog'
```

This issue occurs if the application is created before patching is called. It means our ASGI `TracedMiddleware` has not been added and so we never setup `scope["datadog"]`.

No changelog entry added because this is a fix for a change that has not been released yet.

## Checklist
- [ ] Library documentation is updated.
- [ ] [Corp site](https://github.com/DataDog/documentation/) documentation is updated (link to the PR).

## Reviewer Checklist
- [x] Title is accurate.
- [x] Description motivates each change.
- [x] No unnecessary changes were introduced in this PR.
- [x] PR cannot be broken up into smaller PRs.
- [x] Avoid breaking [API](https://ddtrace.readthedocs.io/en/stable/versioning.html#interfaces) changes unless absolutely necessary.
- [x] Tests provided or description of manual testing performed is included in the code or PR.
- [x] Release note has been added for fixes and features, or else `changelog/no-changelog` label added.
- [x] All relevant GitHub issues are correctly linked.
- [x] Backports are identified and tagged with Mergifyio.
- [ ] Add to milestone.

ddtrace/contrib/starlette/patch.py
@@ -1,4 +1,7 @@
+from typing import Any
+from typing import Dict
 from typing import List
+from typing import Optional
 
 import starlette
 from starlette.middleware import Middleware
@@ -68,7 +71,16 @@ def unpatch():
 def traced_handler(wrapped, instance, args, kwargs):
     # Since handle can be called multiple times for one request, we take the path of each instance
     # Then combine them at the end to get the correct resource names
-    scope = get_argument_value(args, kwargs, 0, "scope")
+    scope = get_argument_value(args, kwargs, 0, "scope")  # type: Optional[Dict[str, Any]]
+    if not scope:
+        return wrapped(*args, **kwargs)
+
+    # Our ASGI TraceMiddleware has not been called, skip since
+    # we won't have a request span to attach this information onto
+    # DEV: This can happen if patching happens after the app has been created
+    if "datadog" not in scope:
+        log.warning("datadog context not present in ASGI request scope, trace middleware may be missing")
+        return wrapped(*args, **kwargs)
 
     # Add the path to the resource_paths list
     if "resource_paths" not in scope["datadog"]:

tests/contrib/starlette/test_starlette.py
@@ -413,3 +413,40 @@ def test_table_query_snapshot(snapshot_client):
     r_get = snapshot_client.get("/notes")
     assert r_get.status_code == 200
     assert r_get.text == "[{'id': 1, 'text': 'test', 'completed': 1}]"
+
+
+@snapshot()
+def test_incorrect_patching(run_python_code_in_subprocess):
+    """
+    When Starlette is patched after the app is created
+        We create no traces
+        We do not crash the application
+        We log a warning
+    """
+    code = """
+from starlette.responses import PlainTextResponse
+from starlette.routing import Route
+from starlette.testclient import TestClient
+import sqlalchemy
+
+from ddtrace import patch_all
+from tests.contrib.starlette.app import get_app
+
+engine = sqlalchemy.create_engine("sqlite:///test.db")
+app = get_app(engine)
+
+# Calling patch_all late
+# DEV: The test client uses `requests` so we want to ignore them for this scenario
+patch_all(requests=False)
+
+with TestClient(app) as test_client:
+    r = test_client.get("/200")
+
+    assert r.status_code == 200
+    assert r.text == "Success"
+    """
+
+    out, err, status, _ = run_python_code_in_subprocess(code)
+    assert status == 0, err
+    assert out == b"", err
+    assert err == b"datadog context not present in ASGI request scope, trace middleware may be missing\n"